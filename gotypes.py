b463d8a68a8ba19e31900d2abc69044bbe067f51
sleung852 <sleung852@gmail.com>
2020-11-21 19:09:54 +0800

    initialisation

---
+++ gotypes.py
@@ -0,0 +1,22 @@
+import enum
+
+class Player(enum.Enum):
+    black = 1
+    white = 2
+
+    @property
+    def other(self):
+        return Player.black if self == Player.white else Player.white
+
+from collections import nametuple
+
+# namedtuple allows access to coordinates as point.row
+# and point.col instead of point[0], point[1]
+class Point(namedtuple('Point', 'row col')):
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1)
+        ]
